15aee38d096d1b0106a932fd751d4bcccf5a18d9
GF <gunhoflash@gmail.com>
2019-06-03 03:56:09 +0900

    Make it faster and stronger!

--- src.cpp
+++ src.cpp
@@ -4,42 +4,30 @@
 		University of Seoul, Department of Computer Science and Engineering
 		2019-1 Computer Algorithm - Team 3
 
-	[ Run Example ]
-		input:
-			2
-			 1  2  3  4  5
-			 6  7  8  9 10
-			11 12 14 19 15
-			16 17 25 13 20
-			21 22 18 23 24
-			 1  2  3  4  5
-			 6  7  8  9 10
-			11 12 13 14 15
-			16 17 18 19 20
-			21 22 24 23 25
-		output:
-			#1 7 R U L D D R R
-			#2 -1
-
 */
 #include <stdio.h>
 #include <stdlib.h>
-#define QUEUE_LENGTH_MAX 65536
+#define QUEUE_LENGTH_MAX 524288
 #define HOLE 25
 #define U 'U'
 #define D 'D'
 #define R 'R'
 #define L 'L'
 
-// 120-byte size
+int score_coefficient;
+int search_bound;
+
+// 152-byte size
 struct pstate
 {
 	bool visit;
 	char type;           // U, D, R, L, or NULL
-	int m[5][5];         // 1 ~ 24 and HOLE
-	int different;       // maximum is 120..?
+	int i;               // position of HOLE
+	int j;               // position of HOLE
+	int m[5][6];         // 1 ~ 24 and HOLE
+	int different;
 	int move;
-	int score;           // = different + move
+	int score;
 	pstate* prev = NULL; // previous state
 } typedef pstate;
 
@@ -47,7 +35,7 @@ struct pstate
 void solution_check();
 
 // print the solution
-void print_solution(int attempt, char solution_list[], int solution_length)
+void print_solution(int attempt, char *solution_list, int solution_length)
 {
 	int i;
 	printf("#%d %d", attempt, solution_length);
@@ -57,12 +45,9 @@ void print_solution(int attempt, char solution_list[], int solution_length)
 }
 
 // update the solution
-void update_solution(pstate* state, char solution_list[], int *solution_length)
+void update_solution(pstate* state, char *solution_list, int *solution_length)
 {
 	pstate* s = state;
-
-	// handle null-pointer-exception
-	if (s == NULL) return;
 
 	// not a short route
 	if (*solution_length != -1 && s->move >= *solution_length) return;
@@ -119,7 +104,11 @@ void calculate_state(pstate *state)
 		{
 			m = state->m[i][j];
 			if (m == HOLE)
+			{
 				state->different += 8 - i - j;
+				state->i = i;
+				state->j = j;
+			}
 			else
 			{
 				t1 = (m - 1) / 5 - i;
@@ -130,13 +119,25 @@ void calculate_state(pstate *state)
 			}
 		}
 
-	state->score = state->different + state->move;
+	state->score = state->different * score_coefficient + (state->move << 6);
 }
 
-void add_new_state(pstate *states[QUEUE_LENGTH_MAX], int *states_length, pstate* prev, char type, int i, int j)
+// find next states, set moves, and calculate score
+void add_new_state(pstate **states, int *states_length, pstate* prev, char type, int solution_length)
 {
-	int k, l;
-	pstate *state = states[*states_length] = (pstate*)malloc(sizeof(pstate));
+	int i, j, k, l;
+	pstate* state;
+
+	// get the position of HOLE
+	i = prev->i;
+	j = prev->j;
+
+	// allocate memory
+	while (states[*states_length] == NULL)
+		states[*states_length] = (pstate*)malloc(sizeof(pstate));
+
+	// initialize the state
+	state = states[*states_length];
 	state->visit = false;
 	state->type = type;
 	for (k = 0; k < 5; k++)
@@ -149,29 +150,57 @@ void add_new_state(pstate *states[QUEUE_LENGTH_MAX], int *states_length, pstate*
 		case L: state->m[i][j] = state->m[i][j - 1]; state->m[i][j - 1] = HOLE; break;
 		case R: state->m[i][j] = state->m[i][j + 1]; state->m[i][j + 1] = HOLE; break;
 	}
+	for (k = 0; k < 5; k++)
+	{
+		state->m[k][5] =
+			state->m[k][0] << 20 ^
+			state->m[k][1] << 15 ^
+			state->m[k][2] << 10 ^
+			state->m[k][3] << 5 ^
+			state->m[k][4];
+	}
 	state->move = prev->move + 1;
 	calculate_state(state);
 	state->prev = prev;
 
+	// add only possibly-short state
+	if (solution_length != -1)
+		if (state->move + state->different / 2 >= solution_length) return;
+
+	// add only new state
+	k = *states_length;
+	while (k--)
+	{
+		if (states[k]->different != state->different) continue;
+		if (states[k]->m[2][5] != state->m[2][5]) continue;
+		if (states[k]->m[3][5] != state->m[3][5]) continue;
+		if (states[k]->m[1][5] != state->m[1][5]) continue;
+		if (states[k]->m[4][5] != state->m[4][5]) continue;
+		if (states[k]->m[0][5] == state->m[0][5]) return;
+	}
+
 	*states_length = *states_length + 1;
 }
 
-void set_next_state(pstate *states[QUEUE_LENGTH_MAX], int states_length, pstate **state, int solution_length)
+void set_next_state(pstate **states, int states_length, pstate **state, int solution_length)
 {
 	int i = states_length;
 	pstate *next_state = NULL;
 	while (i--)
 	{
-		if (states[i] == NULL) continue;
-		if (states[i] == *state) continue;
 		if (states[i]->visit) continue;
+		if (states[i] == *state) continue;
 		if (solution_length != -1 && states[i]->move >= solution_length) continue;
 		if (next_state == NULL) next_state = states[i];
-		else if (next_state->score > states[i]->score)
-			next_state = states[i];
-		else if (next_state->score == states[i]->score)
-			if (next_state->move > states[i]->move)
+		else
+		{
+			if (next_state->score > states[i]->score)
 				next_state = states[i];
+			else if (next_state->score == states[i]->score)
+				if (next_state->move > states[i]->move)
+					next_state = states[i];
+			if (i < search_bound) break;
+		}
 	}
 	*state = next_state;
 }
@@ -181,19 +210,35 @@ void state_test(int attempt, pstate *first)
 	int i, j;
 	
 	// solution
-	char solution_list[QUEUE_LENGTH_MAX] = { 0 };
+	char* solution_list = NULL;
 	int solution_length = -1;
 
 	// states
 	pstate *state = first;
-	pstate *states[QUEUE_LENGTH_MAX] = { NULL };
+	pstate **states = NULL;
 	int states_length = 1;
+
+	// initialize global variables
+	score_coefficient = 64;
+	search_bound = 0;
+
+	// allocate memory and initialize
+	while (solution_list == NULL)
+		solution_list = (char*)malloc(sizeof(char) * QUEUE_LENGTH_MAX);
+	while (states == NULL)
+		states = (pstate **)malloc(sizeof(pstate*) * QUEUE_LENGTH_MAX);
+	for (i = 0; i < QUEUE_LENGTH_MAX; i++)
+	{
+		solution_list[i] = NULL;
+		states[i] = NULL;
+	}
 
 	// set the first state
 	state->type = NULL;
 	state->move = 0;
 	state->prev = NULL;
 	calculate_state(state);
+	states[0] = state;
 
 	// check state solvability
 	if (isSolvable(*state))
@@ -210,27 +255,34 @@ void state_test(int attempt, pstate *first)
 			{
 				update_solution(state, solution_list, &solution_length);
 				set_next_state(states, states_length, &state, solution_length);
+				if (states_length > 100000) break;
 				continue;
 			}
 
 			// no more searching
 			if (states_length + 5 > QUEUE_LENGTH_MAX) break;
 
-			// check the position of hole
-			for (i = 24; i >= 0; i--)
-				if (state->m[i / 5][i % 5] == HOLE)
-				{
-					j = i % 5;
-					i = i / 5;
-					break;
-				}
+			// add next states
+			if (solution_length == -1 || state->move + state->different / 2 < solution_length)
+			{
+				// set i and j as the position of HOLE
+				i = state->i;
+				j = state->j;
 
-			// find next states, set moves, and calculate score
-			if (i < 4 && state->type != U) add_new_state(states, &states_length, state, D, i, j);
-			if (j < 4 && state->type != L) add_new_state(states, &states_length, state, R, i, j);
-			if (i > 0 && state->type != D) add_new_state(states, &states_length, state, U, i, j);
-			if (j > 0 && state->type != R) add_new_state(states, &states_length, state, L, i, j);
+				if (i < 4 && state->type != U) add_new_state(states, &states_length, state, D, solution_length);
+				if (j < 4 && state->type != L) add_new_state(states, &states_length, state, R, solution_length);
+				if (i > 0 && state->type != D) add_new_state(states, &states_length, state, U, solution_length);
+				if (j > 0 && state->type != R) add_new_state(states, &states_length, state, L, solution_length);
+			}
 
+			// tune
+			if (states_length > 20000)
+			{
+				score_coefficient = 32 + (states_length >> 9);
+				search_bound = states_length * 0.75;
+				if (solution_length >= 0 && states_length > 100000) break;
+			}
+			
 			// pick a state with lowest score
 			set_next_state(states, states_length, &state, solution_length);
 		}
@@ -242,26 +294,37 @@ void state_test(int attempt, pstate *first)
 void puzzleSolution()
 {
 	int i, j, k, t;
-	pstate *states;
+	pstate *states = NULL;
 
+	// get the number of test case
 	scanf("%d", &t);
+
+	// execute solution check mode
 	if (t <= 0)
 	{
 		solution_check();
 		return;
 	}
 
-	states = (pstate*)malloc(sizeof(pstate) * t);
-	if (states == NULL)
-	{
-		printf("Memory Allocation Failed. Please try again.\n");
-		exit(-1);
-	}
+	// allocate memory
+	while (states == NULL)
+		states = (pstate*)malloc(sizeof(pstate) * t);
 
+	// get all test cases
 	for (i = 0; i < t; i++)
 		for (j = 0; j < 5; j++)
+		{
 			for (k = 0; k < 5; k++)
 				scanf("%d", &(states[i].m[j][k]));
+			states[i].m[j][5] =
+				states[i].m[j][0] << 20 ^
+				states[i].m[j][1] << 15 ^
+				states[i].m[j][2] << 10 ^
+				states[i].m[j][3] << 5 ^
+				states[i].m[j][4];
+		}
+
+	// start to find solutions
 	for (i = 0; i < t; i++)
 		state_test(i + 1, &states[i]);
 }
